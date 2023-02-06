17491736201f1cfc07e86086188fc0ff6b1fa56f
cherie1214 <cheriey1214@gmail.com>
2023-02-06 01:46:57 -0800

    장바구니 버튼 컴포넌트 추가

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,20 +2,25 @@ import { Fragment } from 'react';
 
 import mealsImage from '../../assets/meals.jpg'
 import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
 
 
 const Header = () => {
-    return (
-        <Fragment>
-            <header className={classes.header}>
-                <h1>Food Order App</h1>
-                <button>장바구니</button>
-            </header>
-            <div className={classes['main-image']}>
-                <img src={mealsImage} alt="테이블에 맛있는 음식이 가득!" />
-            </div>
-        </Fragment>
-    );
+  const onClickHandler = () => {
+    console.log("button clicked!")
+  }
+
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Food Order App</h1>
+        <HeaderCartButton type='button' onClick={onClickHandler}>장바구니</HeaderCartButton>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="테이블에 맛있는 음식이 가득!" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
