401f9d1ad471db73352c06888a92679750484a10
cherie1214 <cheriey1214@gmail.com>
2023-02-06 01:20:14 -0800

    Layout > Header 추가

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,12 +7,12 @@ import classes from './Header.module.css'
 const Header = () => {
     return (
         <Fragment>
-            <header>
+            <header className={classes.header}>
                 <h1>Food Order App</h1>
                 <button>장바구니</button>
             </header>
-            <div>
-                <img src={mealsImage} />
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt="테이블에 맛있는 음식이 가득!" />
             </div>
         </Fragment>
     );
