371cd0d0bbb275dc13d3c5ba7f33edac3088aa40
cherie1214 <cheriey1214@gmail.com>
2023-02-10 20:03:00 +0900

    리듀서 로직 추가하기: 장바구니에 아이템이 없는 경우/이미 있는 경우

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -7,9 +7,10 @@ import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
 
+  // arr.reduce((prev, current) => {}), 누적값의 초기값)  ### 원하는 시작점부터 모든 배열의 요소들을 돌면서 어떤 값을 누적할 때 사용한다.(앞 요소부터)
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
-  }, 0); // 시작 값
+  }, 0);
 
   return (
     <button
