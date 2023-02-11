79430294db5a6b6613d72bf29e4b6d7097453710
cherie1214 <cheriey1214@gmail.com>
2023-02-11 16:16:57 +0900

    장바구니에 아이템 추가될 때 버튼에 에니메이션 추가하기

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,23 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsBumped, sestBtnIsBumped] = useState(false);
+
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
   // arr.reduce((prev, current) => {}), 누적값의 초기값)  ### 원하는 시작점부터 모든 배열의 요소들을 돌면서 어떤 값을 누적할 때 사용한다.(앞 요소부터)
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsBumped ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    sestBtnIsBumped(true);
+
+    const timer = setTimeout(() => {
+      sestBtnIsBumped(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button
-      className={classes.button}
-      type={props.type}
-      onClick={props.onClick}
-    >
+    <button className={btnClasses} type={props.type} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
