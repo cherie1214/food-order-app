6d2efe09d796cb27e87c1f2141dfa55b4e94b296
cherie1214 <cheriey1214@gmail.com>
2023-03-05 01:59:50 +0900

    양식 유효성 검사 추가하기

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,8 +1,18 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import classes from "./Checkout.module.css";
 
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
+
 const Checkout = (props) => {
+  const [formInputValidity, setFormInputValidity] = useState({
+    name: true,
+    city: true,
+    street: true,
+    postal: true,
+  });
+
   const nameInputRef = useRef();
   const cityInputRef = useRef();
   const streetInputRef = useRef();
@@ -10,31 +20,75 @@ const Checkout = (props) => {
 
   const confirmHandler = (event) => {
     event.preventDefault();
-    console.log("확인");
 
     const enteredName = nameInputRef.current.value;
     const enteredCity = cityInputRef.current.value;
     const enteredStreet = streetInputRef.current.value;
     const enteredPostal = postalInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredPostalIsValid =
+      !isEmpty(enteredPostal) && isFiveChars(enteredPostal);
+
+    setFormInputValidity({
+      name: enteredNameIsValid,
+      city: enteredCityIsValid,
+      street: enteredStreetIsValid,
+      postal: enteredPostalIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredCityIsValid &&
+      enteredStreetIsValid &&
+      enteredPostalIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    console.log("모든 입력이 유효한 상태!");
   };
+
+  const nameControlClasses = `${classes.control} ${
+    formInputValidity.name ? "" : classes.invalid
+  }`;
+
+  const cityControlClasses = `${classes.control} ${
+    formInputValidity.city ? "" : classes.invalid
+  }`;
+
+  const streetControlClasses = `${classes.control} ${
+    formInputValidity.street ? "" : classes.invalid
+  }`;
+
+  const postalControlClasses = `${classes.control} ${
+    formInputValidity.postal ? "" : classes.invalid
+  }`;
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
-        <label htmlFor="name">성함</label>
+      <div className={nameControlClasses}>
+        <label htmlFor="name">이름</label>
         <input type="text" id="name" ref={nameInputRef} />
+        {!formInputValidity.name && <p>이름을 입력하세요.</p>}
       </div>
-      <div className={classes.control}>
-        <label htmlFor="city">도시</label>
+      <div className={cityControlClasses}>
+        <label htmlFor="city">도시명</label>
         <input type="text" id="city" ref={cityInputRef} />
+        {!formInputValidity.city && <p>도시명을 입력하세요.</p>}
       </div>
-      <div className={classes.control}>
+      <div className={streetControlClasses}>
         <label htmlFor="street">도로명</label>
         <input type="text" id="street" ref={streetInputRef} />
+        {!formInputValidity.street && <p>도로명을 입력하세요.</p>}
       </div>
-      <div className={classes.control}>
+      <div className={postalControlClasses}>
         <label htmlFor="postal">우편번호</label>
         <input type="text" id="postal" ref={postalInputRef} />
+        {!formInputValidity.postal && <p>우편번호을 입력하세요.</p>}
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancle}>
