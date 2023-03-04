070b5322bcb7b145b633bdcd2c52521a0c1d1e9b
cherie1214 <cheriey1214@gmail.com>
2023-03-05 01:37:47 +0900

    useRef 추가하기

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,30 +1,40 @@
-import React from "react";
+import { useRef } from "react";
 
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const cityInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
     console.log("확인");
+
+    const enteredName = nameInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = postalInputRef.current.value;
   };
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">성함</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">도시</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">도로명</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">우편번호</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalInputRef} />
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancle}>
