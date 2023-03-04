68a9e81722de258bd0f8812207a6c272c1a0d447
cherie1214 <cheriey1214@gmail.com>
2023-03-04 23:16:58 +0900

    meals 데이터 fetch로 가져오기

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,44 @@
+import { useState, useEffect } from "react";
 import Card from "../UI/Card";
 
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchMeals = async function () {
+      setError(null);
+      try {
+        const response = await fetch(
+          "https://react-http-39ce8-default-rtdb.firebaseio.com/meals.json"
+        );
+        if (!response.ok) {
+          throw new Error("무언가 잘못 되었다!");
+        }
+        const data = await response.json();
+        const loadedMeals = [];
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+        setMeals(loadedMeals);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchMeals();
+  }, [error]);
+  console.log(meals);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
